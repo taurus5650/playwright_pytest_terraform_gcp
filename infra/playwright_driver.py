e89553c32d2cc000f77a695b52593b0e2b42cf1a
taurus5650 <taurus_5650@hotmail.com>
2025-07-29 22:14:49 +0800

    Feat: driver phase1

---
+++ infra/playwright_driver.py
@@ -0,0 +1,34 @@
+from playwright.sync_api import sync_playwright, Page, Browser, BrowserContext, TimeoutError
+from utils import logger
+import os
+import time
+
+
+class PlaywrightDriver:
+
+    TIMEOUT = 10000
+
+    def __init__(self, headless: bool = True):
+        self.playwright = sync_playwright().start()
+        self.browser = self.playwright.chromium.launch(headless=headless)
+        self.context = self.browser.new_context()
+        self.page = self.context.new_page()
+
+    def _handle_exception_screenshot(self, action: str, exception: str):
+        logger.error(f'{action} failed: {exception}')
+        self.screenshot()
+        raise exception
+
+    def goto(self, url: str):
+        logger.info(f'goto={url}')
+        try:
+            self.page.goto(url=url)
+        except Exception as e:
+            self._handle_exception_screenshot(action='goto', exception={e})
+
+    def click(self, selector: str, timeout: int = self.TIMEOUT):
+        logger.info(f'click={selector}')
+        try:
+            self.page.locator(selector=selector).click(timeout=timeout)
+        except Exception as e:
+            self._handle_exception_screenshot(action='click', exception={e})
