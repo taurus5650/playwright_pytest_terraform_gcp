27f1b8d0f00049ccca192e26d14ae5468bf454ee
taurus5650 <taurus_5650@hotmail.com>
2025-07-31 23:50:01 +0800

    Feat: expected_sign_up_form_text

--- test_suite/user_team/test_register_user.py
+++ test_suite/user_team/test_register_user.py
@@ -2,6 +2,7 @@ import allure
 
 from infra.playwright_driver import PlaywrightDriver
 from page.home_page.home_page import HomePage
+from page.login_page.login_page import LoginPage
 
 
 class TestCase:
@@ -9,6 +10,7 @@ class TestCase:
     def setup_method(self):
         self.driver = PlaywrightDriver(headless=True)
         self.home = HomePage(driver=self.driver)
+        self.login = LoginPage(driver=self.driver)
 
     def teardown_method(self):
         self.driver.close_driver()
@@ -17,6 +19,7 @@ class TestCase:
     def test_new_register_user(self):
         for_loop_max = 180
         expected_homepage_title = "Automation Exercise"
+        expected_sign_up_form_text = "New User Signup!"
 
         # region __Step1. Navigate to url
         actual_homepage_title = self.home.go_to_hompage()
@@ -28,4 +31,5 @@ class TestCase:
         self.home.click_signup_login_href()
         # region __Step2. Click on 'Signup / Login' button
 
-
+        actual_sign_up_form_text = self.login.inner_text_signin_or_signup_h2()
+        assert actual_sign_up_form_text == expected_sign_up_form_text
