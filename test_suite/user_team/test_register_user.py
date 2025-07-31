4dfefb1b8d83622db8c58f9d9d94ec03cf5753d2
taurus5650 <taurus_5650@hotmail.com>
2025-07-31 23:29:47 +0800

    Feat: 1st assertion

--- test_suite/user_team/test_register_user.py
+++ test_suite/user_team/test_register_user.py
@@ -1,18 +1,13 @@
-import time
-from datetime import datetime
-
 import allure
 
-
-from page.home_page.home_page import HomePage
 from infra.playwright_driver import PlaywrightDriver
-from utils import logger
+from page.home_page.home_page import HomePage
 
 
 class TestCase:
 
     def setup_method(self):
-        self.driver = PlaywrightDriver(headless=False)
+        self.driver = PlaywrightDriver(headless=True)
         self.home = HomePage(driver=self.driver)
 
     def teardown_method(self):
@@ -20,4 +15,17 @@ class TestCase:
 
     @allure.story('New Register User')
     def test_new_register_user(self):
+        for_loop_max = 180
+        expected_homepage_title = "Automation Exercise"
+
+        # region __Step1. Navigate to url
+        actual_homepage_title = self.home.go_to_hompage()
+        # endregion __Step1. Navigate to url
+
+        assert expected_homepage_title in actual_homepage_title.title()
+
+        # region __Step2. Click on 'Signup / Login' button
         self.home.click_signup_login_href()
+        # region __Step2. Click on 'Signup / Login' button
+
+
