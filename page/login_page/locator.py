85ba5960bf142b62dadf57e7e986efceddbfdd3c
taurus5650 <taurus_5650@hotmail.com>
2025-08-02 12:52:07 +0800

    Fix: SIGN_IN_OR_SIGN_UP_PAGE_H2

--- page/login_page/locator.py
+++ page/login_page/locator.py
@@ -1,6 +1,6 @@
 class Locator(object):
 
-    SIGN_IN_OR_SIGN_UP_PAGE_H2 = '(//h2)[3]'  # XPath
+    SIGN_IN_OR_SIGN_UP_PAGE_H2 = 'h2:has-text("New User Signup!")'  # CSS + Text selector (Only Playwright)
     SIGN_UP_NAME = '[data-qa="signup-name"]'  # CSS_SELECTOR
     SIGN_UP_EMAIL = '[data-qa="signup-email"]'  # CSS_SELECTOR
     SIGN_UP_BTN = '[data-qa="signup-button"]'  # CSS_SELECTOR
